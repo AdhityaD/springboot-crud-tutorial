f6e433fd98bd37599dca674e89b808d73c1f7118
Adhitya Wicaksono <>
2020-09-28 20:22:01 +0700

    add swagger

--- src/main/java/com/javaspring/springbootcrud/SpringBootCrudApplication.java
+++ src/main/java/com/javaspring/springbootcrud/SpringBootCrudApplication.java
@@ -2,7 +2,10 @@ package com.javaspring.springbootcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+@EnableSwagger2
 @SpringBootApplication
 public class SpringBootCrudApplication {
 
